cf12750290190099a0a2f2921c29145ce9d979b7
zizoumunoz <zmunoz@outlook.es>
2023-01-29 07:18:25 -0500

    CSS and File writing now function correctly

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-document.getElementById("myBtn").addEventListener("click"), toast();
+function toast(event){
+	const data = new URLSearchParams();
+	var xhttp = new XMLHttpRequest();
+	var formData = new FormData(event.target);
+	var name = formData.get('name');
+	var path = "server.js";
+	for (const pair of formData) {
+    data.append(pair[0], pair[1]);
+}
+    xhttp.open("POST", "http://localhost:8000/app.js", true);
+	xhttp.send(data);
+	
+  }
--- server.js
+++ server.js
@@ -1,41 +1,34 @@
-
 var http = require('http'),
-    fs = require('fs');
-qs = require('querystring')
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err;
-    }
-    http.createServer(function (request, response) {
-        response.writeHeader(200, { "Content-Type": "text/html" });
-        response.write(html);
-        response.end();
-        if (request.method === 'POST') {
-            var data = '';
-            var body = '';
-            request.on('data', function (data) {
-                body += data;
-            });
-            request.on('end', function () {
-                var post = qs.parse(body);
-                toast(post);
-            });
-        }
-    }).listen(8000);
-});
+	fs = require('fs');
+	qs = require('querystring')
+	
+	
+http.createServer(function (req, res) {
+		if (req.method === 'POST'){
+			var data = '';
+			var body = '';
+			req.on('data', function(data){
+				body += data;
+			});
+			req.on('end', function(){
+				var post = qs.parse(body);
+				toast(post);
+			});
+		}
+}).listen(5500);
 
 
 
-function toast(post) {
-    content = post
-    console.log('toast method reached');
-    if (post.foo == 'Waffle') {
-        fs.appendFile('text.txt', post.test1, function (err) {
-            if (err) {
-            }
-            console.log('written');
-            console.log(post.test1);
-        });
-    }
-
+function toast(post){
+	content = post
+	console.log('toast method reached');
+	console.log(content);
+	fs.appendFile('text.txt', post.test1, function (err){
+	if (err){
+	}
+	console.log('written');
+	console.log(post.test1);
+	});
+	
+	
 }
